764f797639ed1749454942fda93a45dbdc5b3ef3
SomeGuy04 <radin.kohani@gmail.com>
2021-11-22 14:35:29 +0330

    added feature to ask for test initiation in order to prevent entering user answers as right results

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def enterToContinue():
 
 conditionsMet=False
 while not conditionsMet:
-    start=int(input("enter the starting question number"))
-    end=int(input("enter the last question number"))
-    step=int(input("enter step number"))
+    start=int(input("enter the starting question number : "))
+    end=int(input("enter the last question number : "))
+    step=int(input("enter step number : "))
     if end<=start:
         print("error : incorrect numbers")
     else:
@@ -16,16 +16,23 @@ CurrentTest=test(start,end,step)
 #setting correct answers
 conditionsMet=True
 while conditionsMet:
-    print("enter THE CORRECT answer for test number ",CurrentTest.getQPos()," ( ",CurrentTest.getNavPos()+1,"/",CurrentTest.getTestLen(),") : ")
+    print("enter THE CORRECT answer for test number ",CurrentTest.getQPos()," ( ",CurrentTest.getNavPos()+1,"/",CurrentTest.getTestLen(),") : ",end='')
     CurrentTest.setRAns(input())
     conditionsMet=CurrentTest.nextPos()
+#making sure user is not zoned out while entering answers
+conditionsMet=False
+while not conditionsMet:
+    if input("type \'begin\' to begin : ")=="begin":
+        conditionsMet=True
+    else:
+        print("i guess you're zoned out and didnt even read this")
 #entering the testing phase
 done=False
 while not done:
     CurrentTest.resetNav()
     conditionsMet=True
     while conditionsMet:
-        print("enter YOUR answer for test number ",CurrentTest.getQPos()," ( ",CurrentTest.getNavPos()+1,"/",CurrentTest.getTestLen(),") : ")
+        print("enter YOUR answer for test number ",CurrentTest.getQPos()," ( ",CurrentTest.getNavPos()+1,"/",CurrentTest.getTestLen(),") : ",end='')
         CurrentTest.setUAns(input())
 
         CurrentQuestionResult=CurrentTest.getQstat(CurrentTest.getNavPos())
@@ -38,7 +45,7 @@ while not done:
         elif CurrentQuestionResult=='s':
             print("skipped")
         else:
-            print("error occured while checking answered")
+            print("error occured while checking answer")
         enterToContinue()
         conditionsMet=CurrentTest.nextPos()
     # results screen
@@ -47,11 +54,10 @@ while not done:
         print("results\ncorrect : ",CurrentTest.getCorrectNumber(),"\nwrong : ",CurrentTest.getWrongNumber(),"\nunanswered : ",CurrentTest.getUnansweredNumber(),
         "\nskipped",CurrentTest.getSkippedNumber(),"\nwrite exit to exit,redo to redo the answering,showCorrect,showWrong,showUnAns,showSkipped")
         endCmd=input()
+        endCmdDone=True
         if endCmd=="exit":
             done=True
-            endCmdDone=True
         elif endCmd=="redo":
-            endCmdDone=True
             done=False
         else:
             endCmdDone=False
