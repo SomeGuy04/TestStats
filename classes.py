13eb5b28c4dca6241e00821763a79d1c028e9bb2
SomeGuy04 <radin.kohani@gmail.com>
2021-12-03 18:41:05 +0330

    configure classes for forceskip

--- classes.py
+++ classes.py
@@ -46,6 +46,10 @@ class test:
         self.rlist[self.navigator]=val
     def setUAns(self,val):  #set users answer for question 
         self.ulist[self.navigator]=val
+    def getRAns(self,pos=navigator):  #set the right answer for question 
+        return self.rlist[pos]
+    def getUAns(self,pos=navigator):  #set users answer for question 
+        return self.ulist[pos]
     def getQPos(self):      #return accual number of the current question
         return self.qpos
     def getTestLen(self):   #return length of test
